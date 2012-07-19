797a690b3b8815458c9eea414a2d197dde0b3d3c
Liz Bennett <embennet@oberlin.edu>
2012-07-19 16:54:03 -0400

    Starting writing the card/deck functionality. It's not currently in a stable state, not sure what the dealio is, will figure it out later.

--- src/com/example/gofish/GameRoomActivity.java
+++ src/com/example/gofish/GameRoomActivity.java
@@ -9,6 +9,9 @@ public class GameRoomActivity extends Activity {
 	private static final int REQUEST_CODE = 10;
 	private static int num_players = 1;
 	private static String player_name = "Liz";
+	private static String deck_size = "Big";
+	
+	private static Deck deck;
 
 	protected void onCreate(Bundle savedInstanceState) {
 		// Be sure to call the super class.
@@ -21,6 +24,7 @@ public class GameRoomActivity extends Activity {
 		startActivityForResult(intent, REQUEST_CODE);
 		// need to set up the game room here
 		setUpGameRoom();
+		buildDeck();
 	}
 
 	@Override
@@ -32,6 +36,7 @@ public class GameRoomActivity extends Activity {
 						&& data.hasExtra("Num Players")) {
 					player_name = data.getExtras().getString("Player Name");
 					num_players = data.getExtras().getInt("Num Players");
+					deck_size = data.getExtras().getString("Deck Size");
 					Toast.makeText(
 							this,
 							"Playing as " + player_name + " with "
@@ -42,5 +47,16 @@ public class GameRoomActivity extends Activity {
 		}
 	}
 	private void setUpGameRoom() {
+		//need cards		
 	}
+	private void buildDeck(){
+		if(deck_size == "Big"){
+			
+		}
+		else{
+			
+		}
+		
+	}
+	/*Please don't break, eclipse!!*/
 }
