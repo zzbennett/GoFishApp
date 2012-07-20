a7328feb40e8b69c6844082ccb585c775633baeb
Liz Bennett <embennet@oberlin.edu>
2012-07-20 18:40:31 -0400

    Merged some conflicts with master branch...not sure what that was about...

--- src/com/example/gofish/Constants.java
+++ src/com/example/gofish/Constants.java
@@ -2,16 +2,8 @@ package com.example.gofish;
 
 public interface Constants {
 	
-<<<<<<< HEAD
 	public static final int SPADES = 1;
 	public static final int HEARTS = 2;
 	public static final int CLUBS = 3;
 	public static final int DIAMONDS = 4;
-=======
-	public static final int HEARTS = 1;
-	public static final int SPADES = 2;
-	public static final int DIAMONDS = 3;
-	public static final int CLUBS = 4;
->>>>>>> branch 'master' of https://github.com/zzbennett/GoFishApp.git
-
 }
